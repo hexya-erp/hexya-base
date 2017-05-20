22e262752de60210cbae2ff1c33f1b7da15620ba
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2017-05-20 23:47:38 +0200

    Added methods execution permission Removed model ACLs

--- base/tests/group_test.go
+++ base/tests/group_test.go
@@ -15,16 +15,19 @@ import (
 func TestGroupLoading(t *testing.T) {
 	models.SimulateInNewEnvironment(security.SuperUserID, func(env models.Environment) {
 		var (
-			adminUser, user     pool.ResUsersSet
-			adminGrp, someGroup pool.ResGroupsSet
+			adminUser, user                    pool.ResUsersSet
+			adminGrp, someGroup, everyoneGroup pool.ResGroupsSet
 		)
 		Convey("Testing Group Loading", t, func() {
 			pool.ResGroups().NewSet(env).ReloadGroups()
 			groups := pool.ResGroups().NewSet(env).FetchAll()
 			So(groups.Len(), ShouldEqual, len(security.Registry.AllGroups()))
 			adminUser = pool.ResUsers().Search(env, pool.ResUsers().ID().Equals(security.SuperUserID))
-			So(adminUser.Groups().Ids(), ShouldHaveLength, 1)
-			So(adminUser.Groups().ID(), ShouldEqual, groups.ID())
+			adminGrp = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupAdminID))
+			everyoneGroup = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupEveryoneID))
+			So(adminUser.Groups().Ids(), ShouldHaveLength, 2)
+			So(adminUser.Groups().Ids(), ShouldContain, adminGrp.ID())
+			So(adminUser.Groups().Ids(), ShouldContain, everyoneGroup.ID())
 		})
 		Convey("Testing Group ReLoading with a new group", t, func() {
 			security.Registry.NewGroup("some_group", "Some Group")
@@ -34,7 +37,8 @@ func TestGroupLoading(t *testing.T) {
 		})
 		Convey("Creating a new user with a new group", t, func() {
 			adminUser = pool.ResUsers().Search(env, pool.ResUsers().ID().Equals(security.SuperUserID))
-			adminGrp = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.AdminGroupID))
+			adminGrp = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupAdminID))
+			everyoneGroup = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupEveryoneID))
 			someGroup = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals("some_group"))
 			user = pool.ResUsers().Create(env, &pool.ResUsersData{
 				Name:   "Test User",
@@ -43,30 +47,41 @@ func TestGroupLoading(t *testing.T) {
 			})
 			So(user.Groups().Ids(), ShouldHaveLength, 1)
 			So(user.Groups().Ids(), ShouldContain, someGroup.ID())
-			So(adminUser.Groups().Ids(), ShouldHaveLength, 1)
+			So(adminUser.Groups().Ids(), ShouldHaveLength, 2)
 			So(adminUser.Groups().Ids(), ShouldContain, adminGrp.ID())
+			So(adminUser.Groups().Ids(), ShouldContain, everyoneGroup.ID())
 		})
 		Convey("Giving the new user admin rights", t, func() {
 			groups := someGroup.Union(adminGrp)
 			user.SetGroups(groups)
-			So(user.Groups().Ids(), ShouldHaveLength, 2)
+			pool.ResGroups().NewSet(env).ReloadGroups()
+			adminGrp = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupAdminID))
+			everyoneGroup = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupEveryoneID))
+			someGroup = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals("some_group"))
+			So(user.Groups().Ids(), ShouldHaveLength, 3)
 			So(user.Groups().Ids(), ShouldContain, someGroup.ID())
 			So(user.Groups().Ids(), ShouldContain, adminGrp.ID())
-			So(adminUser.Groups().Ids(), ShouldHaveLength, 1)
+			So(user.Groups().Ids(), ShouldContain, everyoneGroup.ID())
+			So(adminUser.Groups().Ids(), ShouldHaveLength, 2)
 			So(adminUser.Groups().Ids(), ShouldContain, adminGrp.ID())
+			So(adminUser.Groups().Ids(), ShouldContain, everyoneGroup.ID())
 		})
 		Convey("Removing rights and checking that after reload, we get admin right", t, func() {
+			user = pool.ResUsers().Search(env, pool.ResUsers().Login().Equals("test_user"))
 			user.SetGroups(pool.ResGroups().NewSet(env))
 			adminUser = pool.ResUsers().Search(env, pool.ResUsers().ID().Equals(security.SuperUserID))
 			adminUser.SetGroups(pool.ResGroups().NewSet(env))
 			So(user.Groups().Ids(), ShouldBeEmpty)
 			So(adminUser.Groups().Ids(), ShouldBeEmpty)
 			pool.ResGroups().NewSet(env).ReloadGroups()
-			// We need to reload admin group because it changed id
-			adminGrp = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.AdminGroupID))
-			So(user.Groups().Ids(), ShouldBeEmpty)
-			So(adminUser.Groups().Ids(), ShouldHaveLength, 1)
+			// We need to reload admin and everyone groups because they changed id
+			adminGrp = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupAdminID))
+			everyoneGroup = pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupEveryoneID))
+			So(user.Groups().Ids(), ShouldHaveLength, 1)
+			So(user.Groups().Ids(), ShouldContain, everyoneGroup.ID())
+			So(adminUser.Groups().Ids(), ShouldHaveLength, 2)
 			So(adminUser.Groups().Ids(), ShouldContain, adminGrp.ID())
+			So(adminUser.Groups().Ids(), ShouldContain, everyoneGroup.ID())
 		})
 	})
 }
--- web/tests/relation_test.go
+++ web/tests/relation_test.go
@@ -18,7 +18,7 @@ func Test2ManyRelations(t *testing.T) {
 	Convey("Testing 2many relations modification with client triplets", t, func() {
 		models.SimulateInNewEnvironment(security.SuperUserID, func(env models.Environment) {
 			Convey("Testing many2many '6' triplet", func() {
-				adminGroup := pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.AdminGroupID))
+				adminGroup := pool.ResGroups().Search(env, pool.ResGroups().GroupID().Equals(security.GroupAdminID))
 				jsonGroupsData := fmt.Sprintf("[[6, 0, [%d]]]", adminGroup.ID())
 				var groupsData interface{}
 				json.Unmarshal([]byte(jsonGroupsData), &groupsData)
