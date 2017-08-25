e5d42c05823081a3860c23aef41fe4f0395ecd0a
Nicolas Piganeau <nicolas.piganeau@ndp-systemes.fr>
2017-08-25 18:52:27 +0200

    Cleaning up

--- web/static/src/js/framework/session_instance.js
+++
@@ -1,5 +0,0 @@
-hexya.define('web.session', function (require) {
-    var Session = require('web.Session');
-    var modules = hexya._modules;
-    return new Session(undefined, undefined, {modules:modules, use_cors: false});
-});
